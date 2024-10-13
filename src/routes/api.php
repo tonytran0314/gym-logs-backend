386da5d6e6e1779315d721ee5726a7ce6ec2f9ec
Gia Huy Tran <tonytran0314@gmail.com>
2024-10-12 23:41:14 -0400

    config SPA authentication

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/info', function () {
     return response()->json([
@@ -10,4 +11,18 @@ Route::get('/info', function () {
         'car' => 'Vinfast VF9',
         'motorbike' => 'Honda Winner X 2022',
     ]);
+})->middleware('auth:sanctum');
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        return response()->json([
+            'message' => 'Login successful!',
+        ]);
+    } else {
+        return response()->json([
+            'message' => 'Invalid login credentials!',
+        ], 401);
+    }
 });
